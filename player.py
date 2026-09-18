4fb1c4b61a225ac8ea0d100a27d2e27a0dbd1aa5
pmvt19 <praval.telagi@gmail.com>
2026-09-18 15:35:26 -0700

    Working on the game fixes

--- player.py
+++ player.py
@@ -16,7 +16,8 @@ class CirclePlayer():
 
     # May not be used
     def get_formatted_size(self):
-        return f"{self.size:.2f}"
+        # return f"{self.size:.2f}"
+        return f"{int(self.size)}"
 
     def get_color(self):
         return self.color
@@ -25,7 +26,7 @@ class CirclePlayer():
         self.location = location
 
     def increment_size(self):
-        self.size += 0.1
+        self.size += 0.01
 
     def shoot(self, direction):
         if self.size < self.min_size:
@@ -37,11 +38,11 @@ class CirclePellet():
     def __init__(self, parent: CirclePlayer, direction_vector: np.ndarray):
         self.parent = parent
         self.direction_vector = direction_vector
-        self.location = self._compute_spawn_location()
-
-        self.speed = 1
 
         self.size = 5
+        self.speed = 4
+
+        self.location = self._compute_spawn_location()
 
     def get_size(self):
         return self.size
@@ -54,7 +55,21 @@ class CirclePellet():
 
     def _compute_spawn_location(self):
         radius = self.parent.get_size()
-        return self.parent.get_location() + self.direction_vector * radius
+        return self.parent.get_location() + self.direction_vector * (radius + self.size)
 
     def step_location(self):
         self.location += self.direction_vector * self.speed
+
+
+# Ideas for AI:
+
+# Shoot in the direction of the user circle
+# Shoot in a random direction
+
+
+# Ideas for Items: 
+# Costless Shooting (Shooting pellets doesn't cost size)
+# Instant growth (Though you would have to be careful as to not collide with your own pellets)
+# Speed Boost (Again would require special tuning to not collide with your own pellets)
+
+# Negative Items? - Items you actively want to avoid
